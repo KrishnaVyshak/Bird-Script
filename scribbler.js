2d477bd1c847015599d3524b3d1f468e649f0d76
KrishnaVyshak <79155778+KrishnaVyshak@users.noreply.github.com>
2021-04-23 12:07:07 +0530

    Update scribbler.js

--- scribbler.js
+++ scribbler.js
@@ -12,15 +12,7 @@ var getAll = function (selector, scope) {
 // setup typewriter effect in the terminal demo
 if (document.getElementsByClassName('demo').length > 0) {
   var i = 0;
-  var txt = `scribbler
-            [Entry mode; press Ctrl+D to save and quit; press Ctrl+C to quit without saving]
-
-            ###todo for new year dinner party
-
-            - milk
-            - butter
-            - green onion
-            - lots and lots of kiwis 🥝`;
+  var txt = `PRINT("helllo World!")`;
   var speed = 60;
 
   function typeItOut () {
