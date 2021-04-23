7763bd5f85e57a0ce8df2469554b1e0abee26798
KrishnaVyshak <79155778+KrishnaVyshak@users.noreply.github.com>
2021-04-23 13:44:06 +0530

    Update scribbler.js

--- scribbler.js
+++ scribbler.js
@@ -12,7 +12,7 @@ var getAll = function (selector, scope) {
 // setup typewriter effect in the terminal demo
 if (document.getElementsByClassName('demo').length > 0) {
   var i = 0;
-  var txt = `<span color="#eeeeee">#Creating A Simple Sctipt</span>
+  var txt = `#Creating A Simple Script
 PRINT("ENTER YOUR NAME")
 VAR name = INPUT()
 #Get the name
