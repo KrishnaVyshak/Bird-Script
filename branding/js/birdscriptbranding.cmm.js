a2cdf236f766124b344b499c3c4bf6815a22c29f
KrishnaVyshak <79155778+KrishnaVyshak@users.noreply.github.com>
2021-04-24 12:30:19 +0530

    Update birdscriptbranding.cmm.js

--- branding/js/birdscriptbranding.cmm.js
+++ branding/js/birdscriptbranding.cmm.js
@@ -20,14 +20,14 @@ function load_bs_bs_ic_text_logo_color_48() {
 }
 
 function load_bs_bs_ic_proud_white_48() {
-  document.getElementById("bs_bs_ic_proud_white").src = "http://www.birdscript.tk/branding/bird_script_proud_white_48.png";
+  document.getElementById("bs_bs_ic_proud_white").src = "http://www.birdscript.tk/branding/bird_script_proud_white_48.jpg";
 }
 function load_bs_bs_ic_proud_black_48() {
-  document.getElementById("bs_bs_ic_proud_black").src = "http://www.birdscript.tk/branding/bird_script_proud_black_48.png";
+  document.getElementById("bs_bs_ic_proud_black").src = "http://www.birdscript.tk/branding/bird_script_proud_black_48.jpg";
 }
 
 function load_bs_bs_ic_proud_color_48() {
-  document.getElementById("bs_bs_ic_proud_color").src = "http://www.birdscript.tk/branding/bird_script_proud_color_48.png";
+  document.getElementById("bs_bs_ic_proud_color").src = "http://www.birdscript.tk/branding/bird_script_proud_color_48.jpg";
 }
 
 setTimeout(load_bs_bs_ic_white_48, 200);
