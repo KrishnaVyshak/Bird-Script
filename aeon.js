56ad36295e9d7214684c72d952e6faf3f9e3d63f
VertexDevYT <63761866+AeonTechDev@users.noreply.github.com>
2021-04-25 06:12:45 +0800

    aeon.js

---
+++ aeon.js
@@ -0,0 +1,23 @@
+var i = 0;
+var txt = `#Creating A Simple Script
+PRINT("ENTER YOUR NAME")
+VAR name = INPUT()
+#Get the name
+PRINT("ENTER YOUR AGE")
+VAR age = INPUT_INT()
+#Get the AGE
+PRINT(name)
+PRINT(age)
+#Final Result
+`; /* The text */
+var speed = 80; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+typeWriter();
