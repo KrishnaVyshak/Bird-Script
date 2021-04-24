7bf13585a11c4119ab67eed049ff70e11e054b9c
KrishnaVyshak <79155778+KrishnaVyshak@users.noreply.github.com>
2021-04-24 12:02:36 +0530

    Update birdscriptbranding.cmm.js

--- branding/js/birdscriptbranding.cmm.js
+++ branding/js/birdscriptbranding.cmm.js
@@ -1,5 +1,42 @@
 function load_bs_bs_ic_white_48() {
   document.getElementById("bs_bs_ic_white").src = "http://www.birdscript.tk/branding/bird_script_white_48.png";
 }
+function load_bs_bs_ic_black_48() {
+  document.getElementById("bs_bs_ic_black").src = "http://www.birdscript.tk/branding/bird_script_black_48.png";
+}
+function load_bs_bs_ic_color_48() {
+  document.getElementById("bs_bs_ic_color").src = "http://www.birdscript.tk/branding/bird_script_color_48.png";
+}
+function load_bs_bs_ic_text_logo_white_48() {
+  document.getElementById("bs_bs_ic_txt_white").src = "http://www.birdscript.tk/branding/bird_script_txt_white_48.png";
+}
+
+function load_bs_bs_ic_text_logo_black_48() {
+  document.getElementById("bs_bs_ic_txt_black").src = "http://www.birdscript.tk/branding/bird_script_txt_black_48.png";
+}
+
+function load_bs_bs_ic_text_logo_color_48() {
+  document.getElementById("bs_bs_ic_color").src = "http://www.birdscript.tk/branding/bird_script_text_color_48.png";
+}
+
+function load_bs_bs_ic_proud_white_48() {
+  document.getElementById("bs_bs_ic_proud_white").src = "http://www.birdscript.tk/branding/bird_script_proud_white_48.png";
+}
+function load_bs_bs_ic_proud_black_48() {
+  document.getElementById("bs_bs_ic_proud_black").src = "http://www.birdscript.tk/branding/bird_script_proud_black_48.png";
+}
+
+function load_bs_bs_ic_proud_color_48() {
+  document.getElementById("bs_bs_ic_proud_color").src = "http://www.birdscript.tk/branding/bird_script_proud_color_48.png";
+}
 
 setTimeout(load_bs_bs_ic_white_48, 200);
+setTimeout(load_bs_bs_ic_black_48, 200);
+setTimeout(load_bs_bs_ic_color_48, 200);
+setTimeout(load_bs_bs_ic_text_logo_white_48, 200);
+setTimeout(load_bs_bs_ic_text_logo_black_48, 200);
+setTimeout(load_bs_bs_ic_text_logo_color_48, 200);
+setTimeout(load_bs_bs_ic_proud_color_48, 200);
+setTimeout(load_bs_bs_ic_proud_black_48, 200);
+setTimeout(load_bs_bs_ic_proud_white_48, 200);
+
