980db66cc3921e790ad3d442c9396974ec46e68b
KrishnaVyshak <79155778+KrishnaVyshak@users.noreply.github.com>
2021-04-24 11:22:13 +0530

    Update birdscriptbranding.cmm.js

--- branding/js/birdscriptbranding.cmm.js
+++ branding/js/birdscriptbranding.cmm.js
@@ -1,4 +1,4 @@
 function load_bs_bs_ic_white_48() {
-  document.getElementById("bs_bs_ic_white").src = "bird_script_white_48";
+  document.getElementById("bs_bs_ic_white").src = "/branding/bird_script_white_48";
 }
 load_bs_bs_ic_white_48()
