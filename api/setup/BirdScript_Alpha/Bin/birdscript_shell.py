0227de828602b04f35acdbf0abb89f00ea8a755b
krishna vyshak <79155778+KrishnaVyshak@users.noreply.github.com>
2021-05-26 18:08:31 +0530

    Add files via upload

---
+++ api/setup/BirdScript_Alpha/Bin/birdscript_shell.py
@@ -0,0 +1,16 @@
+import run
+
+while True:
+    text = input('>>> ')
+    if text.strip() == "": continue
+    result, error = run.run('<stdin>', text)
+
+    if error:
+        print(error.as_string())
+    elif result:
+        if len(result.elements) == 1:
+            print(repr(result.elements[0]))
+        else:
+            print(repr(result))
+
+
